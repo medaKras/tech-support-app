93fedb7da34578f88e0fa8bb6a750d3cee4e32d1
medakras <meda.krasauskaite@gmail.com>
2019-10-27 16:29:21 +0200

    With styles

--- src/app/location/support/support.component.ts
+++ src/app/location/support/support.component.ts
@@ -60,93 +60,11 @@ export class SupportComponent implements OnInit {
   initializeMap() {
     this.map = new google.maps.Map(document.getElementById('map'), {
       zoom: 14, 
-      center: this.user.location,
-      styles: [
-        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-        {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-        {
-          featureType: 'administrative.locality',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#d59563'}]
-        },
-        {
-          featureType: 'poi',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#d59563'}]
-        },
-        {
-          featureType: 'poi.park',
-          elementType: 'geometry',
-          stylers: [{color: '#263c3f'}]
-        },
-        {
-          featureType: 'poi.park',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#6b9a76'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'geometry',
-          stylers: [{color: '#38414e'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'geometry.stroke',
-          stylers: [{color: '#212a37'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#9ca5b3'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'geometry',
-          stylers: [{color: '#746855'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'geometry.stroke',
-          stylers: [{color: '#1f2835'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#f3d19c'}]
-        },
-        {
-          featureType: 'transit',
-          elementType: 'geometry',
-          stylers: [{color: '#2f3948'}]
-        },
-        {
-          featureType: 'transit.station',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#d59563'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'geometry',
-          stylers: [{color: '#17263c'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#515c6d'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'labels.text.stroke',
-          stylers: [{color: '#17263c'}]
-        }
-      ]
+      center: this.user.location
     });
     const icon = {
-      url: "../../../assets/user-point.png", // url
-      scaledSize: new google.maps.Size(30, 30), // scaled size
-      origin: new google.maps.Point(0,0), // origin
-      anchor: new google.maps.Point(0, 0) // anchor
+      url: "../../../assets/user-point.png", 
+      scaledSize: new google.maps.Size(25, 25)
     };
     this.userMarker = new google.maps.Marker({position: this.user.location, map: this.map, icon: icon});
   }
@@ -174,9 +92,7 @@ export class SupportComponent implements OnInit {
       if (status == 'OK') {
         const icon = {
           url: "../../../assets/worker-point.png",
-          scaledSize: new google.maps.Size(30, 30), 
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(0, 0) 
+          scaledSize: new google.maps.Size(25, 25)
         };
         
         this.path = result.routes[0].overview_path;
